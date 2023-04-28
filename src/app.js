8f2ca659e3a0aaf4028f0b2105335a6ae3ffd5f9
Burak Cila <b.cila51@gmail.com>
2023-04-28 23:30:51 +0300

    feat: db connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
-import express from "express";
-import dotenv from 'dotenv';
+const express = require('express');
+const config = require('./config');
 
-dotenv.config();
+config(); // DO NOT MOVE DOWN
+const pool = require('./loaders/db');
+
+// loader();
 const app = express();
-
+app.use(express.json());
+app.use('/api', (req, res) => {
+    pool.query('SELECT "hello" FROM DUAL');
+    res.status(200).json({
+        message: 'Welcome to the API',
+    });
+});
 app.listen(process.env.APP_PORT || 8085, () => {
     console.log(`Server running on ${process.env.APP_PORT || 8085}`);
-})
+});
