3544c7815f6f23c98895654b90ad392056c81ed7
Burak Cila <b.cila51@gmail.com>
2023-04-28 22:20:02 +0300

    init 2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.listen(process.env.APP_PORT || 8085, () => {
+    console.log(`Server running on ${process.env.APP_PORT || 8085}`);
+})
