248ca7149971bf1f0236a930edee1ee4ed9659b7
Burak Cila <b.cila51@gmail.com>
2023-05-05 20:06:28 +0300

    chore: added morgan for logging requests

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const config = require('./config');
 config(); // DO NOT MOVE DOWN
 
@@ -8,6 +9,7 @@ const { apiLimiter } = require('./middleware/rateLimiters');
 // loader();
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(apiLimiter);
 
 app.listen(process.env.APP_PORT || 8085, () => {
